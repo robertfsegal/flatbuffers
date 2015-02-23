6ade6cb7a626786af4353d33e7f1c55fb2ab341b
Robert Segal <rob@getsetgames.com>
2015-02-22 19:40:45 -0500

    Xcode project wrapper for inclusion in iOS projects

---
+++ libflatbuffers-ios/libflatbuffers-ios/libflatbuffers_ios.h
@@ -0,0 +1,13 @@
+//
+//  libflatbuffers_ios.h
+//  libflatbuffers-ios
+//
+//  Created by Robert Segal on 2015-02-21.
+//  Copyright (c) 2015 Get Set Games Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libflatbuffers_ios : NSObject
+
+@end
---
+++ libflatbuffers-ios/libflatbuffers-ios/libflatbuffers_ios.m
@@ -0,0 +1,13 @@
+//
+//  libflatbuffers_ios.m
+//  libflatbuffers-ios
+//
+//  Created by Robert Segal on 2015-02-21.
+//  Copyright (c) 2015 Get Set Games Inc. All rights reserved.
+//
+
+#import "libflatbuffers_ios.h"
+
+@implementation libflatbuffers_ios
+
+@end
